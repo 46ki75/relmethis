3836667d839d42d28c7ed23761cf72523f7173ed
shirayuki <shirayuki@46ki75.com>
2024-07-02 04:39:55 +0900

    feat: Create linear loading bar component

---
+++ src/components/data/LineProgress.stories.tsx
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { LineProgress } from './LineProgress'
+import { useState } from 'react'
+
+const meta: Meta<typeof LineProgress> = {
+  title: 'Data/LineProgress',
+  component: LineProgress,
+  tags: ['autodocs'],
+  parameters: {}
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    percent: 50
+  }
+}
+
+export const Colored: Story = {
+  args: {
+    percent: 50,
+    color: 'darkgreen'
+  }
+}
+
+export const Bold: Story = {
+  args: {
+    weight: 8,
+    percent: 50
+  }
+}
+
+export const ExtraBold: Story = {
+  args: {
+    weight: 16,
+    percent: 50
+  }
+}
+
+export const Thin: Story = {
+  args: {
+    weight: 2,
+    percent: 50
+  }
+}
+
+export const Max: Story = {
+  args: {
+    percent: 100
+  }
+}
+
+export const Loading: Story = {
+  args: {
+    percent: 100,
+    isLoading: true,
+    color: 'darkblue'
+  }
+}
+
+export const LoadingToggle: Story = {
+  render: () => {
+    const [isLoading, setIsloasing] = useState(true)
+    return (
+      <>
+        <div>
+          <LineProgress percent={50} isLoading={isLoading} />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setIsloasing(!isLoading)
+            }}
+          >
+            toggle loading
+          </button>
+        </div>
+      </>
+    )
+  }
+}
---
+++ src/components/data/LineProgress.tsx
@@ -0,0 +1,183 @@
+/** @jsxImportSource @emotion/react */
+
+import { css, keyframes } from '@emotion/react'
+import { rgba } from 'polished'
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
+
+// # --------------------------------------------------------------------------------
+//
+// styles
+//
+// # --------------------------------------------------------------------------------
+
+const loadingAnimation = keyframes`
+  0% {
+    background-position: 0% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+`
+
+const lineStyle = ({
+  weight,
+  color,
+  percent,
+  isLoading
+}: {
+  weight: number
+  color: string
+  percent: number
+  isLoading: boolean
+}) =>
+  isLoading
+    ? css`
+        position: relative;
+        width: 100%;
+        height: ${weight}px;
+        background-color: rgb(222, 222, 222);
+        border-radius: 2px;
+
+        &::after {
+          position: absolute;
+          content: '';
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: ${weight}px;
+          border-radius: ${weight / 2}px;
+
+          /* background-color: ${color}; */
+          background: linear-gradient(
+            to right,
+            ${color} 0% 50%,
+            ${rgba(color, 0.25)} 50% 100%
+          );
+          background-size: 200% 100%;
+
+          animation-name: ${loadingAnimation};
+          animation-duration: 1200ms;
+          animation-iteration-count: infinite;
+          animation-fill-mode: both;
+        }
+      `
+    : css`
+        position: relative;
+        width: 100%;
+        height: ${weight}px;
+        background: rgb(222, 222, 222);
+        border-radius: 2px;
+
+        /* BUFFER */
+        &::after {
+          position: absolute;
+          content: '';
+          top: 0;
+          left: 0;
+          width: ${percent}%;
+          height: ${weight}px;
+          background: ${color};
+          border-radius: ${weight / 2}px;
+          opacity: 0.25;
+
+          transition: all 400ms;
+        }
+
+        /* PERCENT */
+        &::before {
+          position: absolute;
+          content: '';
+          top: 0;
+          left: 0;
+          width: ${percent}%;
+          height: ${weight}px;
+          background: ${color};
+          border-radius: ${weight / 2}px;
+
+          transition: all 1600ms;
+        }
+      `
+
+// # --------------------------------------------------------------------------------
+//
+// props
+//
+// # --------------------------------------------------------------------------------
+
+export interface LineProgressProps {
+  /**
+   * **optional** default: 6
+   *
+   * Specify the thickness of the progress bar in pixels.
+   */
+  weight: number
+  /**
+   * **optional** default: 'rgb(22, 22, 22)'
+   *
+   * Specify the color of the progress bar.
+   * The color of the buffer will be calculated and diluted.
+   */
+  color: string
+  /**
+   * **requred**
+   *
+   * The progress of the progress bar is represented in percentage.
+   */
+  percent: number
+  /**
+   * **optional** default: false
+   *
+   * Boolean value indicating whether it is in the process of loading.
+   * During loading, the progress bar animates.
+   */
+  isLoading: boolean
+}
+
+// # --------------------------------------------------------------------------------
+//
+// component
+//
+// # --------------------------------------------------------------------------------
+
+const LineProgressComponent = ({
+  weight,
+  color,
+  percent,
+  isLoading
+}: LineProgressProps) => {
+  const { ref, inView } = useInView({
+    threshold: 0
+  })
+  return (
+    <div
+      ref={ref}
+      css={lineStyle({
+        weight,
+        color,
+        percent: inView ? percent : 0,
+        isLoading
+      })}
+    ></div>
+  )
+}
+
+// # --------------------------------------------------------------------------------
+//
+// default props
+//
+// # --------------------------------------------------------------------------------
+
+LineProgressComponent.defaultProps = {
+  weight: 6,
+  color: 'rgb(22, 22, 22)',
+  isLoading: false
+}
+
+// # --------------------------------------------------------------------------------
+//
+// memoize
+//
+// # --------------------------------------------------------------------------------
+
+export const LineProgress = React.memo(LineProgressComponent)
