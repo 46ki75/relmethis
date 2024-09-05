26ebf430c61535aaf7ad46668a09cb7ed6278acf
shirayuki <shirayuki@46ki75.com>
2024-09-06 00:21:35 +0900

    feat: Add fallback image for Bookmark component

--- src/components/typography/Bookmark.tsx
+++ src/components/typography/Bookmark.tsx
@@ -1,9 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
-import React from 'react'
+import React, { useState } from 'react'
 import { css } from '@emotion/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { DotLoadingIcon } from '../icon/DotLoadingIcon'
 
 // # --------------------------------------------------------------------------------
 //
@@ -38,8 +39,22 @@ const hyperLinkStyle = css`
   }
 `
 
-const imageStyle = css`
+const imageStyle = (isLoading: boolean) => css`
+  ${isLoading && 'width: 0;'}
   height: 100%;
+
+  @media (min-width: 768px) {
+    height: 100%;
+    max-width: 35%;
+    border-radius: 0.25rem 0 0 0.25rem;
+  }
+`
+
+const imageFallbackStyle = (isLoading: boolean) => css`
+  height: 100%;
+  display: ${isLoading ? 'flex' : 'none'};
+  justify-content: center;
+  align-items: center;
 
   @media (min-width: 768px) {
     height: 100%;
@@ -107,10 +122,28 @@ const BookmarkComponent = ({
   url,
   image
 }: BookmarkProps) => {
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <div>
       <a css={hyperLinkStyle} href={url} target='_blank' rel='nopager noopener'>
-        <img css={imageStyle} src={image} alt={description} />
+        <img
+          css={imageStyle(isLoading)}
+          src={image}
+          alt={description}
+          loading='lazy'
+          onLoad={() => {
+            setIsLoading(false)
+          }}
+        />
+
+        <div
+          css={imageFallbackStyle(isLoading)}
+          style={{ width: '35%', height: '128px' }}
+        >
+          <DotLoadingIcon color={'rgba(128,128,128,0.9)'} />
+        </div>
+
         <div css={typographyStyle}>
           <div css={titleStyle}>{title}</div>
           <div css={descriptionStyle}>{description}</div>
