2a289a549ddb33f241de947388910462a967093e
shirayuki <shirayuki@46ki75.com>
2024-09-06 10:19:46 +0900

    feat(Alert): create component

---
+++ src/components/typography/Alert.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Alert } from './Alert'
+import { ImageWithModal } from '../image/ImageWithModal'
+
+const meta: Meta<typeof Alert> = {
+  title: 'Typography/Alert',
+  component: Alert,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: 'radio',
+      options: ['note', 'tip', 'important', 'warning', 'caution']
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: (
+      <div>
+        Useful information that users should know, even when skimming content.
+      </div>
+    ),
+    type: 'note'
+  }
+}
+
+export const Tip: Story = {
+  args: {
+    children: <div>Helpful advice for doing things better or more easily.</div>,
+    type: 'tip'
+  }
+}
+
+export const Important: Story = {
+  args: {
+    children: (
+      <div>Key information users need to know to achieve their goal.</div>
+    ),
+    type: 'important'
+  }
+}
+
+export const Warning: Story = {
+  args: {
+    children: (
+      <div>
+        Urgent info that needs immediate user attention to avoid problems.
+      </div>
+    ),
+    type: 'warning'
+  }
+}
+
+export const Caution: Story = {
+  args: {
+    children: (
+      <div>Advises about risks or negative outcomes of certain actions.</div>
+    ),
+    type: 'caution'
+  }
+}
+
+export const Nest: Story = {
+  args: {
+    children: (
+      <>
+        <p>Advises about risks or negative outcomes of certain actions.</p>
+        <ImageWithModal
+          src='https://images.unsplash.com/photo-1556983703-27576e5afa24?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb'
+          alt=''
+        />
+      </>
+    ),
+    type: 'note'
+  }
+}
---
+++ src/components/typography/Alert.tsx
@@ -0,0 +1,127 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react'
+import {
+  ChartBarSquareIcon,
+  ExclamationTriangleIcon,
+  LightBulbIcon,
+  ShieldCheckIcon,
+  XCircleIcon
+} from '@heroicons/react/24/outline'
+import { darken, rgba } from 'polished'
+import React, { useMemo } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+// # --------------------------------------------------------------------------------
+//
+// style
+//
+// # --------------------------------------------------------------------------------
+
+const wrapperStyle = ({
+  color,
+  inView
+}: {
+  color: string
+  inView: boolean
+}) => css`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  border-left: solid 4px ${darken(0.1, color)};
+  background: linear-gradient(
+    to left,
+    rgba(0, 0, 0, 0) 0% 50%,
+    ${rgba(color, 0.1)} 50% 100%
+  );
+  background-size: 200% 100%;
+  background-position: ${inView ? 0 : 100}%;
+
+  transition: background-position 600ms;
+`
+
+const headerStyle = ({ color }: { color: string }) => css`
+  color: ${darken(0.15, color)};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+`
+
+// # --------------------------------------------------------------------------------
+//
+// props
+//
+// # --------------------------------------------------------------------------------
+
+export type AlertType = 'note' | 'tip' | 'important' | 'warning' | 'caution'
+
+export interface AlertProps {
+  /**
+   * Child components
+   */
+  children: React.ReactNode
+  /**
+   * Type of alert
+   */
+  type: AlertType
+}
+
+const colors: Record<AlertType, string> = {
+  note: '#6987b8',
+  tip: '#59b57c',
+  important: '#9771bd',
+  warning: '#b8a36e',
+  caution: '#b36472'
+}
+
+// # --------------------------------------------------------------------------------
+//
+// component
+//
+// # --------------------------------------------------------------------------------
+
+export const AlertComponent = ({ children, type }: AlertProps) => {
+  const color = useMemo(() => colors[type], [type])
+
+  const icon = useMemo(() => {
+    const style = { width: 20, height: 20 }
+    switch (type) {
+      case 'note':
+        return <ChartBarSquareIcon style={style} />
+      case 'tip':
+        return <LightBulbIcon style={style} />
+      case 'important':
+        return <ShieldCheckIcon style={style} />
+      case 'warning':
+        return <ExclamationTriangleIcon style={style} />
+      case 'caution':
+      default:
+        return <XCircleIcon style={style} />
+    }
+  }, [type])
+
+  const { ref, inView } = useInView()
+
+  return (
+    <div css={wrapperStyle({ color, inView })} ref={ref}>
+      <div css={headerStyle({ color })}>
+        {icon}
+        <div>{type.toUpperCase()}</div>
+      </div>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+// # --------------------------------------------------------------------------------
+//
+// memoize
+//
+// # --------------------------------------------------------------------------------
+
+export const Alert = React.memo(AlertComponent)
