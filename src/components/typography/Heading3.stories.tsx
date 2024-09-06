bf3f7c91c8ace30fa436155444b0dfff5ec854ab
shirayuki <shirayuki@46ki75.com>
2024-09-06 15:48:04 +0900

    feat(Heading3): create component

---
+++ src/components/typography/Heading3.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Heading3 } from './Heading3'
+
+const meta: Meta<typeof Heading3> = {
+  title: 'Typography/Heading3',
+  component: Heading3,
+  tags: ['autodocs'],
+  argTypes: {
+    isDark: { control: 'boolean' }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    text: 'Getting Started',
+    isDark: false
+  }
+}
---
+++ src/components/typography/Heading3.tsx
@@ -0,0 +1,73 @@
+/** @jsxImportSource @emotion/react */
+
+import React from 'react'
+import { css } from '@emotion/react'
+import { FragmentIdentifier } from './FragmentIdentifier'
+
+// # --------------------------------------------------------------------------------
+//
+// styles
+//
+// # --------------------------------------------------------------------------------
+
+const headingStyle = ({ isDark }: { isDark: boolean }) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
+  padding: 0 0 0.25rem 0.5rem;
+
+  position: relative;
+
+  border-left: solid 3px
+    ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
+  border-bottom: dotted 1px
+    ${isDark ? 'rgba(255, 255, 255, 0.2)' : 'rgba(0, 0, 0, 0.2)'};
+`
+
+// # --------------------------------------------------------------------------------
+//
+// props
+//
+// # --------------------------------------------------------------------------------
+
+export interface Heading3Props {
+  /**
+   * The text to be displayed
+   */
+  text: string
+  /**
+   * The ID to be assigned to the tag. Defaults to the value of `text` if not provided
+   */
+  identifier?: string
+  /**
+   * Whether or not to use the dark theme
+   */
+  isDark?: boolean
+}
+
+// # --------------------------------------------------------------------------------
+//
+// component
+//
+// # --------------------------------------------------------------------------------
+
+export const Heading3Component = ({
+  text,
+  identifier = text,
+  isDark = false
+}: Heading3Props) => {
+  return (
+    <>
+      <h3 id={identifier} css={headingStyle({ isDark })}>
+        {text}
+      </h3>
+      <FragmentIdentifier identifier={identifier} />
+    </>
+  )
+}
+
+// # --------------------------------------------------------------------------------
+//
+// memoize
+//
+// # --------------------------------------------------------------------------------
+
+export const Heading3 = React.memo(Heading3Component)
