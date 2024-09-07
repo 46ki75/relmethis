a9c88e023108dc626234e1c5dfade443a536994e
shirayuki <shirayuki@46ki75.com>
2024-09-07 12:09:35 +0900

    feat(InlineCode): Create component

---
+++ src/components/inline/InlineCode.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { InlineCode } from './InlineCode'
+
+const meta: Meta<typeof InlineCode> = {
+  title: 'Components/Inline/InlineCode',
+  component: InlineCode,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    text: 'JSON.stringify()'
+  }
+}
