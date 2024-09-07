2e848d2ec8587f3e5510640654520daa00298596
shirayuki <shirayuki@46ki75.com>
2024-09-08 00:58:41 +0900

    feat(NumberedList): create component

---
+++ src/components/typography/NumberedList.stories.tsx
@@ -0,0 +1,95 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { NumberedList } from './NumberedList'
+import { CodeBlock } from '../code/CodeBlock'
+
+const meta: Meta<typeof NumberedList> = {
+  title: 'Components/Typography/NumberedList',
+  component: NumberedList,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    isDark: false
+  },
+  render: (args) => (
+    <>
+      <p>NumberedList</p>
+      {
+        <NumberedList {...args}>
+          <>List Item 1</>
+          <>List Item 2</>
+          <>List Item 3</>
+        </NumberedList>
+      }
+    </>
+  )
+}
+
+export const Nest: Story = {
+  args: {
+    isDark: false
+  },
+  render: (args) => (
+    <NumberedList {...args}>
+      <>List Item 1</>
+      <>List Item 2</>
+      <NumberedList {...args}>
+        <>List Item 1</>
+        <>List Item 2</>
+        <NumberedList {...args}>
+          <>List Item 1</>
+          <>List Item 2</>
+          <NumberedList {...args}>
+            <>List Item 1</>
+            <>List Item 2</>
+          </NumberedList>
+        </NumberedList>
+      </NumberedList>
+    </NumberedList>
+  )
+}
+
+export const Break: Story = {
+  args: {
+    children: [
+      <>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </>,
+      <>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </>
+    ],
+    isDark: false
+  }
+}
+
+export const Block: Story = {
+  args: {
+    children: [
+      <p>List Item 1</p>,
+      <CodeBlock code='conole.log(0)' language='typescript' />,
+      <p>List Item 3</p>
+    ],
+    isDark: false
+  }
+}
---
+++ src/components/typography/NumberedList.tsx
@@ -0,0 +1,90 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { ReactNode } from 'react'
+import { css } from '@emotion/react'
+
+// # --------------------------------------------------------------------------------
+//
+// styles
+//
+// # --------------------------------------------------------------------------------
+
+const style = ({ isDark }: { isDark: boolean }) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.7)' : 'rgba(0, 0, 0, 0.7)'};
+  box-sizing: border-box;
+  padding-left: 1.25rem;
+
+  li {
+    box-sizing: border-box;
+    padding-left: 0.5rem;
+    margin: 0.75rem auto;
+
+    list-style-type: decimal;
+    &::marker {
+      color: #9771bd;
+    }
+
+    ol {
+      li {
+        list-style-type: lower-alpha;
+        ol {
+          li {
+            list-style-type: lower-roman;
+            ol {
+              li {
+                list-style-type: lower-greek;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  *::selection {
+    background-color: ${isDark
+      ? 'rgba(255, 255, 255, 0.8)'
+      : 'rgba(0, 0, 0, 0.8)'};
+    color: ${isDark ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
+  }
+`
+
+// # --------------------------------------------------------------------------------
+//
+// props
+//
+// # --------------------------------------------------------------------------------
+
+export interface NumberedListProps {
+  children: ReactNode
+  /**
+   * Whether or not to use the dark theme
+   */
+  isDark?: boolean
+}
+
+// # --------------------------------------------------------------------------------
+//
+// component
+//
+// # --------------------------------------------------------------------------------
+
+const NumberedListComponent: React.FC<NumberedListProps> = ({
+  children,
+  isDark = false
+}) => {
+  return (
+    <ol css={style({ isDark })}>
+      {React.Children.map(children, (child, index) => (
+        <li key={index}>{child}</li>
+      ))}
+    </ol>
+  )
+}
+// # --------------------------------------------------------------------------------
+//
+// memoize
+//
+// # --------------------------------------------------------------------------------
+
+export const NumberedList = React.memo(NumberedListComponent)
