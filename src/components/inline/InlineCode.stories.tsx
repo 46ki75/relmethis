2a462a6be452425780c93e867b715c9375b171dc
shirayuki <shirayuki@46ki75.com>
2024-09-07 20:49:25 +0900

    feat(InlineCode): add isDark prop

--- src/components/inline/InlineCode.stories.tsx
+++ src/components/inline/InlineCode.stories.tsx
@@ -12,6 +12,7 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    text: 'JSON.stringify()'
+    text: 'JSON.stringify()',
+    isDark: false
   }
 }
--- src/components/inline/InlineCode.tsx
+++ src/components/inline/InlineCode.tsx
@@ -9,7 +9,8 @@ import { css } from '@emotion/react'
 //
 // # --------------------------------------------------------------------------------
 
-const style = css`
+const style = ({ isDark }: { isDark: boolean }) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
   padding: 0.25rem 0.5rem;
   background-color: rgba(128, 128, 128, 0.1);
   border-radius: 0.25rem;
@@ -27,6 +28,10 @@ export interface InlineCodeProps {
    * The text to be displayed
    */
   text: string
+  /**
+   * Whether or not to use the dark theme
+   */
+  isDark?: boolean
 }
 
 // # --------------------------------------------------------------------------------
@@ -35,8 +40,8 @@ export interface InlineCodeProps {
 //
 // # --------------------------------------------------------------------------------
 
-const InlineCodeComponent = ({ text }: InlineCodeProps) => {
-  return <code css={style}>{text}</code>
+const InlineCodeComponent = ({ text, isDark = false }: InlineCodeProps) => {
+  return <code css={style({ isDark })}>{text}</code>
 }
 
 // # --------------------------------------------------------------------------------
