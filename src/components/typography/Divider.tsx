46badab115635b31d31cdc6e21551f3ca5d5d74e
shirayuki <shirayuki@46ki75.com>
2024-09-09 03:51:54 +0900

    feat(Divider): adjust margin

--- src/components/typography/Divider.tsx
+++ src/components/typography/Divider.tsx
@@ -12,7 +12,7 @@ import { useInView } from 'react-intersection-observer'
 
 const containerStyle = css`
   width: 100%;
-  margin: 1rem 0;
+  margin-block: 1rem;
 
   display: flex;
   flex-direction: row;
