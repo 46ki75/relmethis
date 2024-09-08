9d581e391128cf79d9d04c7a54203d20b95f8201
shirayuki <shirayuki@46ki75.com>
2024-09-09 00:39:27 +0900

    feat(BreadCrumb): create component

---
+++ src/components/navigation/Breadcrumbs.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Breadcrumbs } from './Breadcrumbs'
+import { CloudIcon } from '@heroicons/react/24/outline'
+
+const meta: Meta<typeof Breadcrumbs> = {
+  title: 'Components/Navigation/Breadcrumbs',
+  component: Breadcrumbs,
+  tags: ['autodocs']
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    links: [
+      { href: '/', label: 'Home' },
+      { href: '/blog', label: 'Blog' },
+      { href: '/blog/article', label: 'Article' },
+      { href: '/blog/article/1', label: 'Page' }
+    ],
+    isDark: false,
+    align: 'center'
+  }
+}
+
+export const Colored: Story = {
+  args: {
+    links: [
+      { href: '/', label: 'Home', color: '#59b57c' },
+      { href: '/blog', label: 'Blog', color: '#6987b8' },
+      { href: '/blog/article', label: 'Article', color: '#9771bd' }
+    ],
+    isDark: false,
+    align: 'center'
+  }
+}
+
+export const CustomIcon: Story = {
+  args: {
+    links: [
+      { href: '/', label: 'Home', icon: <>🏠</> },
+      {
+        href: '/blog',
+        label: 'Blog',
+        icon: <CloudIcon style={{ width: 16 }} />
+      },
+      { href: '/blog/article', label: 'Article', icon: <>●</> }
+    ],
+    isDark: false,
+    align: 'center'
+  }
+}
+
+export const AlignLeft: Story = {
+  args: {
+    links: [
+      { href: '/', label: 'Home' },
+      { href: '/blog', label: 'Blog' },
+      { href: '/blog/article', label: 'Article' },
+      { href: '/blog/article/1', label: 'Page' }
+    ],
+    isDark: false,
+    align: 'left'
+  }
+}
---
+++ src/components/navigation/Breadcrumbs.tsx
@@ -0,0 +1,233 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { ReactNode, useCallback, useMemo } from 'react'
+import { css } from '@emotion/react'
+import {
+  ChevronRightIcon,
+  DocumentIcon,
+  FolderOpenIcon,
+  HomeIcon
+} from '@heroicons/react/24/outline'
+import { useInView } from 'react-intersection-observer'
+import { rgba } from 'polished'
+import { isEqual } from 'lodash'
+
+// # --------------------------------------------------------------------------------
+//
+// styles
+//
+// # --------------------------------------------------------------------------------
+
+const wrapperStyle = ({
+  isDark,
+  align
+}: {
+  isDark: boolean
+  align?: 'center' | 'left' | 'right'
+}) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
+
+  display: flex;
+  justify-content: ${align === 'center'
+    ? 'center'
+    : align === 'left'
+      ? 'flex-start'
+      : 'flex-end'};
+  align-items: center;
+  gap: 0.5rem;
+`
+
+const linkStyle = ({
+  inView,
+  delay
+}: {
+  inView: boolean
+  delay: string
+}) => css`
+  all: unset;
+
+  padding: 0.125rem 0.25rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+
+  opacity: ${inView ? 1 : 0};
+  transition:
+    opacity 400ms ease ${delay},
+    background-color 300ms;
+
+  &:hover {
+    background-color: ${rgba('#6987b8', 0.2)};
+  }
+`
+
+const iconStyle = ({ color }: { color: string }) => css`
+  color: ${color};
+  width: 16px;
+  height: 16px;
+`
+
+const animationIconStyle = ({
+  inView,
+  delay
+}: {
+  inView: boolean
+  delay: string
+}) => css`
+  color: rgba(128, 128, 128, 0.8);
+  width: 16px;
+  height: 16px;
+
+  opacity: ${inView ? 1 : 0};
+  transition: opacity 400ms ease ${delay};
+`
+
+// # --------------------------------------------------------------------------------
+//
+// utils
+//
+// # --------------------------------------------------------------------------------
+
+const isExternal = (href: string) => {
+  const currentHost = window.location.hostname
+  const linkUrl = new URL(href, window.location.href)
+  const isExternal = linkUrl.hostname !== currentHost
+  return isExternal
+}
+
+// # --------------------------------------------------------------------------------
+//
+// props
+//
+// # --------------------------------------------------------------------------------
+
+export interface BreadcrumbsProps {
+  style?: React.CSSProperties
+  /**
+   * Array of link information
+   */
+  links: Array<{
+    /**
+     * Text to be displayed
+     */
+    label: string
+    /**
+     * Destination URL
+     */
+    href: string
+    /**
+     * Custom icon that can be passed as a ReactNode
+     */
+    icon?: ReactNode
+    /**
+     * Color of the icon
+     */
+    color?: string
+  }>
+  /**
+   * Whether or not to use the dark theme
+   */
+  isDark?: boolean
+  /**
+   * Position of the breadcrumb. Default is `center`.
+   */
+  align?: 'center' | 'left' | 'right'
+}
+
+// # --------------------------------------------------------------------------------
+//
+// component
+//
+// # --------------------------------------------------------------------------------
+
+const BreadcrumbsComponent = ({
+  style,
+  links,
+  isDark = false,
+  align = 'center'
+}: BreadcrumbsProps) => {
+  const { ref, inView } = useInView()
+
+  const defaultColor = useMemo(
+    () => (isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'),
+    [isDark]
+  )
+
+  const Links = useCallback(
+    (inView: boolean) => {
+      return links.map((link, index) => (
+        <>
+          {/* linktext */}
+
+          <a
+            href={link.href}
+            target={isExternal(link.href) ? '_blank' : undefined}
+            rel={isExternal(link.href) ? 'noopener noreferrer' : undefined}
+            key={link.label + link.href}
+            css={linkStyle({ inView, delay: `${200 * index}ms` })}
+          >
+            {index === 0
+              ? (link.icon ?? (
+                  <HomeIcon
+                    css={iconStyle({ color: link.color ?? defaultColor })}
+                  />
+                ))
+              : index + 1 !== links.length
+                ? (link.icon ?? (
+                    <FolderOpenIcon
+                      css={iconStyle({ color: link.color ?? defaultColor })}
+                    />
+                  ))
+                : (link.icon ?? (
+                    <DocumentIcon
+                      css={iconStyle({ color: link.color ?? defaultColor })}
+                    />
+                  ))}
+
+            <span>{link.label}</span>
+          </a>
+
+          {/* chevron */}
+
+          {index + 1 !== links.length && (
+            <ChevronRightIcon
+              css={animationIconStyle({
+                inView,
+                delay: `${200 * (index * 1) + 100}ms`
+              })}
+            />
+          )}
+        </>
+      ))
+    },
+    [defaultColor, links]
+  )
+
+  return (
+    <div css={wrapperStyle({ isDark, align })} style={style} ref={ref}>
+      {Links(inView)}
+    </div>
+  )
+}
+
+// # --------------------------------------------------------------------------------
+//
+// memoize
+//
+// # --------------------------------------------------------------------------------
+
+export const Breadcrumbs = React.memo(
+  BreadcrumbsComponent,
+  (prevProps, nextProps) => {
+    return (
+      prevProps.isDark === nextProps.isDark &&
+      prevProps.align === nextProps.align &&
+      isEqual(prevProps.links, nextProps.links) &&
+      isEqual(prevProps.style, nextProps.style)
+    )
+  }
+)
