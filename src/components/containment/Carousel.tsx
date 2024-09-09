82e17f96225cd5e142c6eab9f664a3789da1ce3b
shirayuki <shirayuki@46ki75.com>
2024-09-09 17:37:25 +0900

    feat(useCarousel): add method to scroll to specific page

--- src/components/containment/Carousel.tsx
+++ src/components/containment/Carousel.tsx
@@ -1,7 +1,14 @@
 /** @jsxImportSource @emotion/react */
 
 import React, { ReactNode } from 'react'
-import { useCarousel } from './useCarousel'
+import { useCarousel, UseCarouselProps } from './useCarousel'
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon
+} from '@heroicons/react/16/solid'
+import { css } from '@emotion/react'
 
 // # --------------------------------------------------------------------------------
 //
@@ -9,19 +16,70 @@ import { useCarousel } from './useCarousel'
 //
 // # --------------------------------------------------------------------------------
 
+const controlContainere = css`
+  margin-block: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+`
+
+const iconStyle = ({ isDark }: { isDark: boolean }) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.7)' : 'rgba(0, 0, 0, 0.7)'};
+  padding: 0.25rem;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: background-color 200ms;
+
+  &:hover {
+    background-color: rgba(128, 128, 128, 0.25);
+  }
+`
+const controlIndicatorStyle = ({ isDark }: { isDark: boolean }) => css`
+  color: ${isDark ? 'rgba(255, 255, 255, 0.7)' : 'rgba(0, 0, 0, 0.7)'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+  user-select: none;
+`
+
+const dot = ({
+  isDark,
+
+  isActive
+}: {
+  isDark: boolean
+  isActive: boolean
+}) => css`
+  background-color: ${isDark
+    ? 'rgba(255, 255, 255, 0.7)'
+    : 'rgba(0, 0, 0, 0.7)'};
+  opacity: ${isActive ? 0.8 : 0.3};
+  width: ${isActive ? 10 : 6}px;
+  height: ${isActive ? 10 : 6}px;
+  border-radius: 50%;
+`
+
 // # --------------------------------------------------------------------------------
 //
 // props
 //
 // # --------------------------------------------------------------------------------
 
-export interface CarouselProps {
+export interface CarouselProps extends UseCarouselProps {
   children: ReactNode[]
   /**
    * - `true`: The container size automatically adjusts to the size of the displayed page
    * - `false`: The container size is fixed to the largest size
    */
   autoResize?: boolean
+  /**
+   * Whether or not to use the dark theme
+   */
+  isDark?: boolean
 }
 
 // # --------------------------------------------------------------------------------
@@ -30,21 +88,39 @@ export interface CarouselProps {
 //
 // # --------------------------------------------------------------------------------
 
-const CarouselComponent = ({ children, autoResize }: CarouselProps) => {
-  const { currentPage, end, next, prev, start, renderCarousel } = useCarousel({
-    children,
-    autoResize
-  })
+const CarouselComponent = ({
+  children,
+  autoResize,
+  isDark = false
+}: CarouselProps) => {
+  const { currentPage, scrollToPage, end, next, prev, start, renderCarousel } =
+    useCarousel({
+      children,
+      autoResize
+    })
 
   return (
     <>
       {renderCarousel()}
-      <button onClick={start}>START</button>
-      <button onClick={prev}>prev</button>
-      <button onClick={next}>next</button>
-      <button onClick={end}>END</button>
-      <div>
-        現在のページ: {currentPage} / {children.length}
+      <div css={controlContainere}>
+        <ChevronDoubleLeftIcon css={iconStyle({ isDark })} onClick={start} />
+        <ChevronLeftIcon css={iconStyle({ isDark })} onClick={prev} />
+        <div css={controlIndicatorStyle({ isDark })}>
+          <span>{currentPage}</span>
+          <span>/</span>
+          <span>{children.length}</span>
+          <span
+            css={dot({ isDark, isActive: false })}
+            onClick={() => {
+              scrollToPage(2)
+            }}
+          ></span>
+          <span css={dot({ isDark, isActive: false })}></span>
+          <span css={dot({ isDark, isActive: true })}></span>
+          <span css={dot({ isDark, isActive: false })}></span>
+        </div>
+        <ChevronRightIcon css={iconStyle({ isDark })} onClick={next} />
+        <ChevronDoubleRightIcon css={iconStyle({ isDark })} onClick={end} />
       </div>
     </>
   )
