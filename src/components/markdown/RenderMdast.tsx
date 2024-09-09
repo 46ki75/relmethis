dafa93a53c5fda6e1fd34331c0a8046ca23d0354
shirayuki <shirayuki@46ki75.com>
2024-09-09 10:35:17 +0900

    feat(Markdown): create table style

--- src/components/markdown/RenderMdast.tsx
+++ src/components/markdown/RenderMdast.tsx
@@ -22,6 +22,8 @@ import { BulletedList } from '../typography/BulletedList'
 import { Blockquote } from '../typography/Blockquote'
 import { Paragraph } from '../typography/Paragraph'
 
+import { darken, lighten } from 'polished'
+
 import type { CodeBlock as CodeBlockType } from '../code/CodeBlock'
 const CodeBlock = React.lazy(() =>
   import('../code/CodeBlock').then((module) => ({
@@ -44,7 +46,105 @@ const ImageWithModal = React.lazy(() =>
 // # --------------------------------------------------------------------------------
 
 const tableStyle = ({ isDark }: { isDark: boolean }) => css`
-  color: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
+  width: fit-content;
+  max-width: 100%;
+  display: block;
+  overflow-x: auto;
+  scrollbar-width: thin;
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.15);
+
+  &::-webkit-scrollbar {
+    height: 6px;
+    width: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgb(138, 148, 191, 0.8);
+    border-radius: 3px;
+    opacity: 0.6;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: #f3f3f3;
+  }
+
+  th,
+  td {
+    padding: 12px 15px;
+    white-space: nowrap;
+  }
+
+  thead {
+    tr {
+      background-color: ${isDark
+        ? 'rgba(255, 255, 255, 0.8)'
+        : 'rgba(0, 0, 0, 0.8)'};
+
+      text-align: left;
+
+      th {
+        border-right: 1px dashed rgba(128, 128, 128, 0.4);
+
+        * {
+          color: ${isDark ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
+
+          &::selection {
+            background-color: ${!isDark
+              ? 'rgba(255, 255, 255, 0.8)'
+              : 'rgba(0, 0, 0, 0.8)'};
+            color: ${!isDark
+              ? 'rgba(0, 0, 0, 0.8)'
+              : 'rgba(255, 255, 255, 0.8)'};
+          }
+        }
+
+        &:last-child {
+          border-right: none;
+        }
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      border-bottom: 1px solid rgba(128, 128, 128, 0.4);
+
+      transition: background-color 300ms;
+
+      &:nth-of-type(odd) {
+        background-color: ${isDark ? 'rgb(40,40,40)' : 'rgb(240,240,240)'};
+      }
+
+      &:nth-of-type(even) {
+        background-color: ${isDark ? 'rgb(60,60,60)' : 'rgb(250,250,250)'};
+      }
+
+      &:hover {
+        background-color: ${isDark
+          ? darken(0.35, '#6987b8')
+          : lighten(0.35, '#6987b8')};
+      }
+
+      td {
+        border-right: 1px dashed rgba(128, 128, 128, 0.4);
+
+        &:last-child {
+          border-right: none;
+        }
+
+        *::selection {
+          background-color: ${isDark
+            ? 'rgba(255, 255, 255, 0.8)'
+            : 'rgba(0, 0, 0, 0.8)'};
+          color: ${isDark ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
+        }
+      }
+    }
+  }
 `
 
 const supStyle = ({ isDark }: { isDark: boolean }) => css`
