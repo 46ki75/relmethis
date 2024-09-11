1c8644d67027dd2ad633564ec5b28527eaa39bbe
shirayuki <shirayuki@46ki75.com>
2024-09-11 17:24:34 +0900

    chore(vite): add plugin to bundle CSS in JS files

--- src/global.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'relmethis/dist/assets/style.css'
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.app.json' }),
+    cssInjectedByJsPlugin() // Bundling CSS into JS
+  ],
   base: '/relmethis',
   build: {
     lib: {
