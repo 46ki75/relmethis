9ffc82a56ccdb2eaa9876135d691c36f7bc1c198
shirayuki <shirayuki@46ki75.com>
2024-09-11 17:06:06 +0900

    chore(vite): support IntelliSense for CSS paths

---
+++ src/global.d.ts
@@ -0,0 +1,1 @@
+declare module 'relmethis/dist/assets/style.css'
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ tsconfigPath: './tsconfig.app.json' }),
-    cssInjectedByJsPlugin()
-  ],
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
   base: '/relmethis',
   build: {
     lib: {
