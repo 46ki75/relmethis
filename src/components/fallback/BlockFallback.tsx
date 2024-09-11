da50d5d1dfe6d081b7eb6b071000d15d7bbb03be
shirayuki <shirayuki@46ki75.com>
2024-09-12 04:22:06 +0900

    refactor(BlockFallback): rewrite using SCSS module

--- src/components/fallback/BlockFallback.tsx
+++ src/components/fallback/BlockFallback.tsx
@@ -1,25 +1,10 @@
-/** @jsxImportSource @emotion/react */
-
 import React from 'react'
-import { css } from '@emotion/react'
+
 import { DotLoadingIcon } from '../icon/DotLoadingIcon'
 
 import isEqual from 'react-fast-compare'
 
-// # --------------------------------------------------------------------------------
-//
-// styles
-//
-// # --------------------------------------------------------------------------------
-
-const wrapperStyle = css`
-  box-sizing: border-box;
-  padding: 2rem;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+import styles from './BlockFallback.module.scss'
 
 // # --------------------------------------------------------------------------------
 //
@@ -39,7 +24,7 @@ export interface BlockFallbackProps {
 
 const BlockFallbackComponent = ({ style }: BlockFallbackProps) => {
   return (
-    <div css={wrapperStyle} style={style}>
+    <div style={style} className={styles.wrapper}>
       <DotLoadingIcon size={32} color='rgba(128,128,128,0.5)' />
     </div>
   )
