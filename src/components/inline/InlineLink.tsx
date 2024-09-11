8eb9b4257fcdeb7f57c64bdb55ed822b2bfd29d6
shirayuki <shirayuki@46ki75.com>
2024-09-12 06:36:54 +0900

    refactor(InlineLink): Refactor with SCSS Modules

--- src/components/inline/InlineLink.tsx
+++ src/components/inline/InlineLink.tsx
@@ -1,75 +1,14 @@
-/** @jsxImportSource @emotion/react */
-
 import React from 'react'
-import { css, keyframes } from '@emotion/react'
-import { darken, lighten, rgba } from 'polished'
+
 import {
   ArrowTopRightOnSquareIcon,
   ChevronRightIcon
 } from '@heroicons/react/24/outline'
 
 import isEqual from 'react-fast-compare'
+import { useCSSVariable } from '../../hooks/useCSSVariable'
 
-// # --------------------------------------------------------------------------------
-//
-// styles
-//
-// # --------------------------------------------------------------------------------
-
-const style = ({ isDark }: { isDark: boolean }) => css`
-  all: unset;
-  display: inline;
-  cursor: pointer;
-  padding: 0 0.25rem;
-
-  color: ${isDark ? lighten(0.1, '#6987b8') : darken(0.1, '#6987b8')};
-  border-bottom: dashed 1px #6987b8;
-  border-radius: 2px;
-  transition:
-    background-color 200ms,
-    border-bottom 200ms;
-
-  &:hover {
-    background-color: ${rgba(
-      isDark ? lighten(0.1, '#6987b8') : darken(0.1, '#6987b8'),
-      0.2
-    )};
-    border-bottom: dashed 0px rgba(0, 0, 0, 0);
-  }
-
-  &:visited {
-    color: ${isDark ? lighten(0.1, '#9771bd') : darken(0.1, '#9771bd')};
-    &:hover {
-      background-color: ${rgba(
-        isDark ? lighten(0.1, '#9771bd') : darken(0.1, '#9771bd'),
-        0.2
-      )};
-    }
-  }
-
-  span::selection {
-    background-color: ${isDark
-      ? 'rgba(255, 255, 255, 0.8)'
-      : 'rgba(0, 0, 0, 0.8)'};
-    color: ${isDark ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
-  }
-`
-
-const blink = keyframes`
-    from { opacity: 1; }
-    to { opacity: 0.1; }
-`
-
-const iconStyle = css`
-  width: 16px;
-  height: 16px;
-  margin-left: 0.25rem;
-  animation-name: ${blink};
-  animation-direction: alternate;
-  animation-duration: 2s;
-  animation-timing-function: ease-in;
-  animation-iteration-count: infinite;
-`
+import styles from './InlineLink.module.scss'
 
 // # --------------------------------------------------------------------------------
 //
@@ -108,19 +47,25 @@ const InlineLinkComponent = ({
   const linkUrl = new URL(href, window.location.href)
   const isExternal = linkUrl.hostname !== currentHost
 
+  const { ref } = useCSSVariable<HTMLAnchorElement>({
+    '--react-color-fg': isDark
+      ? 'rgba(0, 0, 0, 0.8)'
+      : 'rgba(255, 255, 255, 0.8)',
+    '--react-color-bg': isDark
+      ? 'rgba(255, 255, 255, 0.8)'
+      : 'rgba(0, 0, 0, 0.8)'
+  })
+
   return (
     <a
+      className={styles.wrapper}
+      ref={ref}
       href={href}
-      css={style({ isDark })}
       target={isExternal ? '_blank' : undefined}
       rel={isExternal ? 'noopener noreferrer' : undefined}
     >
       <span>{text}</span>
-      {isExternal ? (
-        <ArrowTopRightOnSquareIcon css={iconStyle} />
-      ) : (
-        <ChevronRightIcon css={iconStyle} />
-      )}
+      {isExternal ? <ArrowTopRightOnSquareIcon /> : <ChevronRightIcon />}
     </a>
   )
 }
