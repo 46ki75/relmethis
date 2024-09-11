f92895f0f3f1146a484f3fd391e48c77111ae0e9
shirayuki <shirayuki@46ki75.com>
2024-09-11 11:01:26 +0900

    feat: Add bulk export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
 import './index.scss'
+
+// # --------------------------------------------------------------------------------
+//
+// components
+//
+// # --------------------------------------------------------------------------------
+
+export * from './components/code/CodeBlock'
+export * from './components/code/KaTex'
+export * from './components/code/Mermaid'
+
+export * from './components/containment/Carousel'
+export * from './components/containment/SimpleTooltip'
+export * from './components/containment/Toggle'
+export * from './components/containment/Tooltip'
+export * from './components/containment/useCarousel'
+
+export * from './components/data/Item'
+export * from './components/data/LineProgress'
+export * from './components/data/MultiLineProgress'
+export * from './components/data/Table'
+
+export * from './components/fallback/BlockFallback'
+export * from './components/fallback/FullscreenFallback'
+export * from './components/fallback/RectangleWave'
+
+export * from './components/form/Checkbox'
+export * from './components/form/Edit'
+
+export * from './components/icon/DotLoadingIcon'
+export * from './components/icon/LanguageIcon'
+export * from './components/icon/SquareLoadingIcon'
+
+export * from './components/image/ImageWithFallback'
+export * from './components/image/ImageWithModal'
+
+export * from './components/inline/InlineCode'
+export * from './components/inline/InlineLink'
+export * from './components/inline/InlineText'
+
+export * from './components/markdown/Markdown'
+export * from './components/markdown/Mdast'
+export * from './components/markdown/RenderMdast'
+export * from './components/markdown/mdastToString'
+
+export * from './components/navigation/BarPagination'
+export * from './components/navigation/Breadcrumbs'
+export * from './components/navigation/DotPagination'
+export * from './components/navigation/Pagetop'
+export * from './components/navigation/TableOfContents'
+
+export * from './components/other/Kbd'
+export * from './components/other/Keyboard'
+
+export * from './components/typography/Alert'
+export * from './components/typography/Blockquote'
+export * from './components/typography/Bookmark'
+export * from './components/typography/BulletedList'
+export * from './components/typography/Divider'
+export * from './components/typography/FragmentIdentifier'
+export * from './components/typography/Heading1'
+export * from './components/typography/Heading2'
+export * from './components/typography/Heading3'
+export * from './components/typography/Heading4'
+export * from './components/typography/Heading5'
+export * from './components/typography/Heading6'
+export * from './components/typography/NumberedList'
+export * from './components/typography/Paragraph'
+
+// # --------------------------------------------------------------------------------
+//
+// hooks
+//
+// # --------------------------------------------------------------------------------
+
+export * from './hooks/useCopy'
+export * from './hooks/useScrollToHash'
