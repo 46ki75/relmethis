3075cb3f1cae41fd8488250a19bd046dbb9827c6
shirayuki <shirayuki@46ki75.com>
2024-09-11 16:54:53 +0900

    chore(vite): add plugin to bundle CSS into JS

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
+import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({ tsconfigPath: './tsconfig.app.json' }),
+    cssInjectedByJsPlugin()
+  ],
   base: '/relmethis',
   build: {
-    cssCodeSplit: false, // Bundling CSS into JS
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'relmethis',
@@ -27,7 +32,6 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
   css: {
     modules: {
       scopeBehaviour: 'local'
