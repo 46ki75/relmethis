1d37225a45f0ae2fa7648bab0b7e048625dc67bb
shirayuki <shirayuki@46ki75.com>
2024-09-11 15:20:52 +0900

    feat(InlineCode): implement color change using CSS variables

--- src/components/inline/InlineCode.stories.tsx
+++ src/components/inline/InlineCode.stories.tsx
@@ -4,7 +4,9 @@ import { InlineCode } from './InlineCode'
 const meta: Meta<typeof InlineCode> = {
   title: 'Components/Inline/InlineCode',
   component: InlineCode,
-  tags: ['autodocs']
+  tags: ['autodocs'],
+  argTypes: { color: { control: 'color' } },
+  args: { isDark: false }
 }
 
 export default meta
@@ -12,7 +14,23 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    text: 'JSON.stringify()',
-    isDark: false
+    text: 'JSON.stringify()'
   }
+}
+
+export const Coloed: Story = {
+  args: {
+    text: 'JSON.stringify()',
+    color: '#59b57c'
+  }
+}
+
+export const Multi: Story = {
+  render: () => (
+    <>
+      <InlineCode text='Hi!' color='red' />
+      <InlineCode text='Hi!' />
+      <InlineCode text='Hi!' />
+    </>
+  )
 }
--- src/components/inline/InlineCode.tsx
+++ src/components/inline/InlineCode.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './InlineCode.module.scss'
 import classNames from 'classnames'
+import { type Property } from 'csstype'
+import { useCSSVariable } from '../../hooks/useCSSVariable'
+import { getLuminance } from 'polished'
 
 // # --------------------------------------------------------------------------------
 //
@@ -17,6 +20,10 @@ export interface InlineCodeProps {
    * Whether or not to use the dark theme
    */
   isDark?: boolean
+  /**
+   * The color of the text
+   */
+  color?: Property.Color
 }
 
 // # --------------------------------------------------------------------------------
@@ -25,9 +32,21 @@ export interface InlineCodeProps {
 //
 // # --------------------------------------------------------------------------------
 
-const InlineCodeComponent = ({ text, isDark = false }: InlineCodeProps) => {
+const InlineCodeComponent = ({
+  text,
+  isDark = false,
+  color = isDark ? 'rgba(255,255,255,0.8)' : 'rgba(0,0,0,0.8)'
+}: InlineCodeProps) => {
+  const { ref } = useCSSVariable({
+    '--react-color-fg': color,
+    '--react-color-bg': color,
+    '--react-color-fg-selection':
+      getLuminance(color) < 0.5 ? 'rgba(255,255,255,0.8)' : 'rgba(0,0,0,0.8)'
+  })
+
   return (
     <code
+      ref={ref}
       className={classNames(styles.code, {
         [styles['react-dark']]: isDark,
         [styles['react-light']]: !isDark
