0b8885cef0bb491c7f09c8c691600579b6695410
shirayuki <shirayuki@46ki75.com>
2024-09-12 04:24:04 +0900

    refactor(FullscreenFallback): rewrite using SCSS module

--- src/components/fallback/FullscreenFallback.tsx
+++ src/components/fallback/FullscreenFallback.tsx
@@ -1,35 +1,13 @@
-/** @jsxImportSource @emotion/react */
-
 import React from 'react'
-import { css } from '@emotion/react'
+
 import { DotLoadingIcon } from '../icon/DotLoadingIcon'
 import { createPortal } from 'react-dom'
-import { darken } from 'polished'
+
 import { RectangleWave } from './RectangleWave'
 
 import isEqual from 'react-fast-compare'
 
-// # --------------------------------------------------------------------------------
-//
-// styles
-//
-// # --------------------------------------------------------------------------------
-
-const wrapperStyle = css`
-  box-sizing: border-box;
-  padding: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  position: fixed;
-  z-index: 100;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${darken(0.05, 'white')};
-`
+import styles from './FullscreenFallback.module.scss'
 
 // # --------------------------------------------------------------------------------
 //
@@ -49,7 +27,7 @@ export interface FullscreenFallbackProps {
 
 const FullscreenFallbackComponent = ({ style }: FullscreenFallbackProps) => {
   return createPortal(
-    <div css={wrapperStyle} style={style}>
+    <div className={styles.wrapper} style={style}>
       <RectangleWave color='rgba(128,128,128,0.8)' />
       <DotLoadingIcon size={64} color='rgba(128,128,128,0.8)' />
     </div>,
