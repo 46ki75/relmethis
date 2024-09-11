9b0b0afb2f80a314253d1d62d0cfb14ee18b822d
shirayuki <shirayuki@46ki75.com>
2024-09-11 12:12:06 +0900

    chore(vite): create settings for generating type definition files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import dts from 'vite-plugin-dts'
+
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/relmethis',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'relmethis',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })]
 })
