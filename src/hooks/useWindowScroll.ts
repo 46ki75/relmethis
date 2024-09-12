177c1acb18ad78266494cf2d916465fd798910dc
shirayuki <shirayuki@46ki75.com>
2024-09-13 02:08:36 +0900

    feat(useWindowScroll): implement throttling

--- src/hooks/useWindowScroll.ts
+++ src/hooks/useWindowScroll.ts
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
+import throttle from 'lodash/throttle'
 
 export interface State {
   x: number
   y: number
 }
 
-export const useWindowScroll = (): State => {
+export const useWindowScroll = (options?: { throttle?: number }): State => {
   const [scrollPosition, setScrollPosition] = useState<State>({
     x: window.scrollX,
     y: window.scrollY
@@ -18,12 +19,20 @@ export const useWindowScroll = (): State => {
     })
   }
 
+  const throttledHandleScroll = options?.throttle
+    ? throttle(handleScroll, options.throttle)
+    : null
+
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
+    const scrollHandler = throttledHandleScroll || handleScroll
+    window.addEventListener('scroll', scrollHandler)
     return () => {
-      window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('scroll', scrollHandler)
+      if (throttledHandleScroll) {
+        throttledHandleScroll.cancel()
+      }
     }
-  }, [])
+  }, [throttledHandleScroll])
 
   return scrollPosition
 }
