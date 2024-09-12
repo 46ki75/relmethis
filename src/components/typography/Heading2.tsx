00a2091006e87e69c8832568a60ac0b9df0870e5
shirayuki <shirayuki@46ki75.com>
2024-09-12 17:51:02 +0900

    refactor(Heading2): Refactor using SCSS modules

--- src/components/typography/Heading2.tsx
+++ src/components/typography/Heading2.tsx
@@ -1,111 +1,20 @@
-/** @jsxImportSource @emotion/react */
-
-import { css } from '@emotion/react'
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
 import { FragmentIdentifier } from './FragmentIdentifier'
 import { useScrollToHash } from '../../hooks/useScrollToHash'
 
 import isEqual from 'react-fast-compare'
+import { useCSSVariable } from '../../hooks/useCSSVariable'
+import { useMergeRefs } from '../../hooks/useMergeRefs'
+
+import styles from './Heading2.module.scss'
+import classNames from 'classnames'
 
 // # --------------------------------------------------------------------------------
 //
 // styles
 //
 // # --------------------------------------------------------------------------------
-
-const topStyle = ({ isDark }: { isDark: boolean }) => css`
-  margin-block-start: 2rem;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-
-  div:nth-of-type(1) {
-    width: 4px;
-    height: 4px;
-    background-color: ${isDark
-      ? 'rgba(255, 255, 255, 0.8)'
-      : 'rgba(0, 0, 0, 0.8)'};
-  }
-
-  div:nth-of-type(2) {
-    width: 20%;
-    height: 1px;
-    border-top: dashed 1px
-      ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
-  }
-`
-
-const headingStyle = ({ isDark }: { isDark: boolean }) => css`
-  margin: 0;
-  position: relative;
-  color: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
-  font-size: 1.5rem;
-
-  &::selection {
-    background: ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
-    color: ${isDark ? 'rgba(0, 0, 0, 0.8)' : 'rgba(255, 255, 255, 0.8)'};
-  }
-
-  &::after {
-    position: absolute;
-    content: '';
-    right: 2px;
-    bottom: 0;
-    width: 6px;
-    height: 8px;
-    background-color: ${isDark
-      ? 'rgba(255, 255, 255, 0.8)'
-      : 'rgba(0, 0, 0, 0.8)'};
-    opacity: 0.8;
-    transform: skewX(-25deg);
-  }
-
-  &::before {
-    position: absolute;
-    content: '';
-    right: 10px;
-    bottom: 0;
-    width: 6px;
-    height: 8px;
-    background-color: ${isDark
-      ? 'rgba(255, 255, 255, 0.8)'
-      : 'rgba(0, 0, 0, 0.8)'};
-    opacity: 0.8;
-    transform: skewX(-25deg);
-  }
-`
-
-const bottomStyle = ({
-  inView,
-  isDark
-}: {
-  inView: boolean
-  isDark: boolean
-}) => css`
-  height: 2px;
-  width: ${inView ? '100%' : '0%'};
-  margin-bottom: 0.5rem;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  border-bottom: solid 1px
-    ${isDark ? 'rgba(255, 255, 255, 0.8)' : 'rgba(0, 0, 0, 0.8)'};
-
-  transition: width 800ms;
-  position: relative;
-
-  &::after {
-    position: absolute;
-    content: '';
-    width: 16px;
-    height: 2px;
-    top: 2px;
-    background-color: ${isDark
-      ? 'rgba(255, 255, 255, 0.8)'
-      : 'rgba(0, 0, 0, 0.8)'};
-  }
-`
 
 // # --------------------------------------------------------------------------------
 //
@@ -139,20 +48,41 @@ const Heading2Component = ({
   identifier = text,
   isDark = false
 }: Heading2Props) => {
-  const { ref, inView } = useInView()
+  const { ref: a, inView } = useInView()
+  const { ref: b } = useCSSVariable({
+    '--react-color-fg': isDark
+      ? 'rgba(255, 255, 255, 0.8)'
+      : 'rgba(0, 0, 0, 0.8)',
+    '--react-color-bg': isDark
+      ? 'rgba(0, 0, 0, 0.8)'
+      : 'rgba(255, 255, 255, 0.8)'
+  })
+  const ref = useMergeRefs(a, b)
 
   useScrollToHash(identifier, 200)
 
   return (
-    <section id={identifier}>
-      <div css={topStyle({ isDark })}>
-        <div></div>
-        <div></div>
+    <section ref={ref} id={identifier} className={styles.section}>
+      <div
+        className={classNames(styles['section__top'], {
+          [styles['section__top--visible']]: inView
+        })}
+      >
+        <div className={styles['section__top-first']}></div>
+        <div className={styles['section__top-second']}></div>
       </div>
-      <h2 ref={ref} css={headingStyle({ isDark })}>
+      <h2
+        className={classNames(styles['section__heading'], {
+          [styles['section__heading--visible']]: inView
+        })}
+      >
         {text}
       </h2>
-      <div css={bottomStyle({ inView, isDark })}></div>
+      <div
+        className={classNames(styles['section__bottom'], {
+          [styles['section__bottom--visible']]: inView
+        })}
+      ></div>
       <FragmentIdentifier identifier={identifier} isDark={isDark} />
     </section>
   )
