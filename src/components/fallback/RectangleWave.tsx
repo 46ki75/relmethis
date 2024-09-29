de08de12b0fc2d83f2d2652ff1de4f67224408cb
shirayuki <shirayuki@46ki75.com>
2024-09-29 12:30:06 +0900

    feat(ReacangleWave): revert to SCSS Modules

--- src/components/fallback/RectangleWave.tsx
+++ src/components/fallback/RectangleWave.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 
 import isEqual from 'react-fast-compare'
 import { useCSSVariable } from '../../hooks/useCSSVariable'
 
-import css from './RectangleWave.css?inline'
+import styles from './RectangleWave.module.scss'
 
 // # --------------------------------------------------------------------------------
 //
@@ -30,18 +29,9 @@ const RectangleWaveComponent = ({
   isDark = false,
   color = isDark ? 'rgba(255,255,255,0.6)' : 'rgba(0,0,0,0.6)'
 }: RectangleWaveProps): JSX.Element => {
-  const CLASSNAME = 'com-46ki75-react-relmethis-rectangle-wave'
-
   const { ref } = useCSSVariable({ '--react-color': color })
 
-  return (
-    <>
-      <Helmet>
-        <style key={CLASSNAME}>{css}</style>
-      </Helmet>
-      <div ref={ref} className={CLASSNAME}></div>
-    </>
-  )
+  return <div ref={ref} className={styles['rectangle-wave']}></div>
 }
 
 // # --------------------------------------------------------------------------------
