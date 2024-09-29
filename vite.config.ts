0ebff539d471424b134ccfbf04953e5db84e9505
shirayuki <shirayuki@46ki75.com>
2024-09-30 00:35:56 +0900

    build(vite): enable SSR option

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 import UnoCSS from 'unocss/vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import preserveDirectives from 'rollup-preserve-directives'
 
 import path from 'path'
 
@@ -13,7 +14,8 @@ export default defineConfig({
     react(),
     dts({ tsconfigPath: './tsconfig.app.json' }),
     UnoCSS(),
-    cssInjectedByJsPlugin() // Bundling CSS into JS
+    cssInjectedByJsPlugin(), // Bundling CSS into JS
+    preserveDirectives() // keep 'use client'
   ],
   base: '/relmethis',
   build: {
@@ -24,15 +26,20 @@ export default defineConfig({
       formats: ['es', 'cjs']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime'
         },
         assetFileNames: `assets/[name].[ext]`
       }
-    }
+    },
+    minify: false,
+    ssr: true,
+    ssrEmitAssets: true,
+    ssrManifest: true
   },
   css: {
     modules: {
