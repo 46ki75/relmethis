2c15dd663618d673308a734cfbe396b23f1621e3
shirayuki <shirayuki@46ki75.com>
2024-10-02 03:49:42 +0900

    feat(useInView): create custom hook

---
+++ src/hooks/useInViewById.ts
@@ -0,0 +1,26 @@
+'use cilent'
+
+import { useEffect, useState } from 'react'
+
+export const useInView = (elementId: string) => {
+  const [isInView, setIsInView] = useState(false)
+
+  useEffect(() => {
+    const element = document.getElementById(elementId)
+    if (!element) return
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        setIsInView(entry.isIntersecting)
+      })
+    })
+
+    observer.observe(element)
+
+    return () => {
+      observer.unobserve(element)
+    }
+  }, [elementId])
+
+  return isInView
+}
